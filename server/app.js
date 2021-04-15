ad5049a0bc82f0fc476cca5e17a1015e18130f10
Ibrahim Heritch <her.ibrahim@hotmail.com>
2021-04-15 10:46:44 -0400

    adeed reservation router to server

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var addTestRouter = require('./routes/test');
+var reservationRouter = require('./routes/reservation')
 
 var cors = require("cors");
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Add routes here
 app.use('/', indexRouter);
 app.use('/test', addTestRouter);
+app.use('/reservation',reservationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
