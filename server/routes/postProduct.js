dac4290f2695743b8d62b5dbc6ed9e3a7cc7aa19
AzharAlalawi <54441499+AzharAlalawi@users.noreply.github.com>
2021-04-19 22:54:11 -0400

    Add post function

--- server/routes/postProduct.js
+++ server/routes/postProduct.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 ////TODO: make this function add a product
 router.post("/", function (req, res, next) {
-    console.log(req.body);
+    database.executeQuery(
+        `INSERT INTO product (id, name, description, product_type, quantity, price, rating, picture)
+            value('1' `, req.body.productTitle + ', ' + req.body.description + ', ' + ', ' + req.body.selectProductCategory + ', ' +
+                    req.body.quantity + ', ' + req.body.productPrice + `, '5.0', 'NULL')`
+    );
 });
 
 module.exports = router;
