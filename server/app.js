1485daa21831e7f648508b34a13b04df9f9e8bd1
AzharAlalawi <54441499+AzharAlalawi@users.noreply.github.com>
2021-04-20 23:00:45 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ var postProductRouter = require('./routes/postProduct')
 var signupRouter = require('./routes/signup')
 var loginRouter = require('./routes/login')
 var searchRouter = require('./routes/search')
+var productsRouter = require('./routes/productsPage')
 
 var cors = require("cors");
 
@@ -21,6 +22,10 @@ var database = require("./database/database.js");
 database.init();
 
 var app = express();
+
+//for delete button
+
+const methodOverride = require('method-override')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,6 +39,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+//For delete method
+app.use(methodOverride('_method'))
+
 //Add routes here
 app.use('/', indexRouter);
 app.use('/test', addTestRouter);
@@ -42,6 +50,9 @@ app.use('/login', loginRouter);
 app.use('/reservation',reservationRouter);
 app.use('/postProduct', postProductRouter);
 app.use('/Search', searchRouter);
+app.use('/productsPage', productsRouter);
+
+
 
 
 // catch 404 and forward to error handler
